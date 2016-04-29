d4d48644057d8fa20443e956320ad05451ecbf60
Steven Ao <Steven.Ao@D2L.com>
2016-04-28 18:03:11 -0700

    fix ToDelimitedString encoding problem when there is &/= in the dictionary key or value

--- src/Owin.Security.Saml/IDictionaryExtensions.cs
+++ src/Owin.Security.Saml/IDictionaryExtensions.cs
@@ -1,36 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Owin.Security.Saml
 {
     public static class IDictionaryExtensions
     {
-        public static NameValueCollection ToNameValueCollection<TKey, TValue>(this IDictionary<TKey, TValue> value)
+        public static NameValueCollection ToNameValueCollection(this IDictionary<string, string> value)
         {
             if (value == null) throw new ArgumentNullException("value");
             var nvc = new NameValueCollection(value.Count);
             foreach (var item in value)
-                nvc.Add(item.Key.ToString(), item.Value.ToString());
+                nvc.Add(item.Key, item.Value);
             return nvc;
         }
 
-        public static string ToDelimitedString<TKey, TValue>(this IDictionary<TKey, TValue> value)
+        public static string ToDelimitedString(this IDictionary<string, string> value)
         {
-            if (value == null) throw new ArgumentNullException("value");
-            return string.Join("&", value.Select(kvp => string.Format("{0}={1}", kvp.Key, kvp.Value)));
+            if (value == null) throw new ArgumentNullException("value");	
+            return JsonSerialize( value );
         }
 
         public static IEnumerable<KeyValuePair<string,string>> FromDelimitedString(this string value)
         {
             if (value == null) throw new ArgumentNullException("value");
-            return value.Split('&').Select(kvp => {
-                var split = kvp.Split('=');
-                return new KeyValuePair<string, string>(split[0], split[1]);
-            });
+			IDictionary<string, string> dict = JsonDeserialize( value );
+			return dict.ToList();
         }
+
+		private static string JsonSerialize( IDictionary<string, string> value ) 
+		{
+			var ser = new DataContractJsonSerializer( typeof( IDictionary<string, string> ) );
+			using( var ms = new MemoryStream() ) 
+			{
+				ser.WriteObject( ms, value );
+				var jsonString = Encoding.UTF8.GetString( ms.ToArray() );
+				return jsonString;
+			}
+		}
+
+		private static IDictionary<string, string> JsonDeserialize(string value) 
+		{
+			var ser = new DataContractJsonSerializer( typeof( IDictionary<string, string> ) );
+			using(var ms = new MemoryStream( Encoding.UTF8.GetBytes( value ) )) 
+			{
+				var obj = (IDictionary<string, string>) ser.ReadObject( ms );
+				return obj;
+			}
+		}
     }
 }
